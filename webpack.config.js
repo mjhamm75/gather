698d3a9c0ed4f44e25d7dad0da01ce4e49980be3
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-08 19:50:08 -0600

    Get Hello World working with React

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          loader: 'babel',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ],
     }
 }

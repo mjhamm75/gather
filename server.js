941a80634bb1194a80533d388d0c859c9b460350
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-11 09:17:53 -0600

    Update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var PORT = 5000 || process.env.PORT;
+var PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/dist'));
 

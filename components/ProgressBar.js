90f97962b584f9fada743445d4098b7780b91b5f
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-15 18:50:00 -0600

    add classnames

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
 import s from './ProgressBar.css';
 
 const ProgressBar = ({needed, received}) => {
   let percent = (received / needed) * 100;
+  let barStyle = {
+    width: `${percent - 2}%`
+  }
+  let barClass = classnames(s.bar, {
+    [s.high]: percent === 100,
+    [s.middle]: percent >= 40 && percent < 100,
+    [s.low]: percent > 0 && percent <= 39
+  })
   return (
     <div className={s.flex}>
       <div className={s.progress}>
-        <span className={s.bar} style={{'width': `${percent}%`}}></span>
+        <span className={barClass} style={barStyle}></span>
       </div>
     </div>
   )

efbfa370fc5a49198036f0518faaa17be1d2befc
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-20 19:01:07 -0600

    Style out send reminder to all

--- components/SendReminderToAll.js
+++ components/SendReminderToAll.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Bell from './Bell';
+
 import s from './SendReminderToAll.css';
 
 export default () => (
-  <div className={s.reminder}>Send reminder to all</div>
+  <div className={s.reminder}>
+    <span className={s.text}>Send reminder to all</span>
+    <Bell />
+  </div>
 )

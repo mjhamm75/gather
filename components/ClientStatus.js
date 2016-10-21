e0b2b3dbaf56649f27d583235e8dff81a0737076
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-20 19:10:18 -0600

    Create legend at top of client table

--- components/ClientStatus.js
+++ components/ClientStatus.js
@@ -3,5 +3,18 @@ import React from 'react';
 import s from './ClientStatus.css';
 
 export default () => (
-  <div className={s.clientStatus}>Status</div>
+  <div className={s.clientStatus}>
+    <div className={s.box}>
+      <div className={s.redBox}></div>
+      <div className={s.text}>29 Need Documents</div>
+    </div>
+    <div className={s.box}>
+      <div className={s.yellowBox}></div>
+      <div className={s.text}>3 Partly Complete</div>
+    </div>
+    <div className={s.box}>
+      <div className={s.greenBox}></div>
+      <div className={s.text}>2 Complete</div>
+    </div>
+  </div>
 )

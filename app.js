12bd7d584086954f92a4eae9100d7505a8beacb6
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-22 11:12:38 -0600

    Add create route

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import Page from './components/Page';
+
 import ClientManagement from './components/ClientManagement';
+import CreateNewClient from './components/CreateNewClient';
+
 import reducer from './reducers'
 
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 
 let store = createStore(reducer);
 
@@ -15,6 +18,7 @@ render((
   <Provider store={store}>
     <Router history={browserHistory}>
       <Router path="/" component={Page}>
+        <Route path="create" component={CreateNewClient}/>
         <IndexRoute component={ClientManagement}/>
       </Router>
     </Router>
---
+++ components/CreateNewClient.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreateNewClient extends Component {
+  render() {
+    return (
+      <h1>New Account</h1>
+    )
+  }
+}
+
+export default CreateNewClient;

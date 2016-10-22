aeb68aff51ba3b4e72eedca17b8a19ac24750b54
mjhamm75 <jason.hamm@ldschurch.org>
2016-10-22 08:55:52 -0600

    Go to create route when clicking create new account

--- components/ClientManagement.js
+++ components/ClientManagement.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import ClientList from './ClientList';
 import ClientSearch from './ClientSearch';
@@ -9,13 +10,22 @@ import SendReminderToAll from './SendReminderToAll';
 import s from './ClientManagement.css';
 
 class ClientManagement extends Component {
+  createNewAccount() {
+    console.log('create new account');
+  }
+
   render() {
     return (
       <div className={s.clientManagementFrame}>
         <div className={s.frameBar}>
           <div className={s.active}>Active (3)</div>
           <div className={s.archived}>Archived (122)</div>
-          <div className={s.link}>+ Create New Account</div>
+          <div className={s.createNew}>
+            <Link
+              className={s.link}
+              to={`/create`}
+            >+ Create New Account</Link>
+          </div>
         </div>
         <div className={s.clientManagement}>
           <div className={s.tableActionBar}>
@@ -31,6 +41,7 @@ class ClientManagement extends Component {
     )
   }
 }
+
 export default connect(state => ({
   clients: state.clients
 }))(ClientManagement);

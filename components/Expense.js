b13ee7bf785c9b31c8eef39821f54594b8e3225c
mjhamm75 <jason.hamm@ldschurch.org>
2016-11-03 21:21:15 -0600

    Add expenses

--- components/Expense.js
+++ components/Expense.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Select from 'react-select';
 
-export default () => {
+import s from './Expense.css';
+
+export default ({title, options}) => {
   return (
-    <div>Document Request</div>
+    <div className={s.expense}>
+      <div className={s.title}>{title}</div>
+      <Select
+        options={options}
+        placeholder="- Request documents -"
+      />
+      <div className={s.group}>
+        <input type="checkbox"/><label>Mortgage</label>
+      </div>
+      <div className={s.group}>
+        <input type="checkbox"/><label>Car Payment</label>
+      </div>
+      <div className={s.group}>
+        <input type="checkbox"/><label>Utilities</label>
+      </div>
+    </div>
   )
 }
--- components/Expenses.js
+++ components/Expenses.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import s from './Expenses.css';
+
 import Expense from './Expense';
 
 export default () => {
   return (
-    <div>
-      <h2>Expenses</h2>
-      <Expense />
+    <div className={s.expenses}>
+      <div className={s.title}>Request for Documentation</div>
+      <div className={s.expenseGroup}>
+        <Expense
+          title="Monthly Expenses"
+          />
+        <Expense
+          title="Income"
+          />
+        <Expense
+          title="Taxes"
+          />
+      </div>
     </div>
   )
 }

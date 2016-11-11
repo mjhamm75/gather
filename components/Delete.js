656c74b05755024daa6a674e01c075a51eaf4c8a
mjhamm75 <jason.hamm@ldschurch.org>
2016-11-10 22:18:47 -0700

    Add delete icon

---
+++ components/Delete.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default ({onClick}) => (
+  <svg width="14px" height="14px" viewBox="0 0 14 14" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" onClick={onClick}>
+      <g id="Hamm&amp;Hamm" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round">
+          <g id="Account-Creation" transform="translate(-426.000000, -1027.000000)" strokeWidth="2">
+              <path d="M427.5,1028.5 L438.813708,1039.81371" id="Line-Copy-8" transform="translate(433.156854, 1034.156854) scale(-1, 1) translate(-433.156854, -1034.156854) "></path>
+          </g>
+      </g>
+      <g id="Hamm&amp;Hamm" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round">
+          <g id="Account-Creation" transform="translate(-426.000000, -1027.000000)" strokeWidth="2">
+              <path d="M427.5,1028.5 L438.813708,1039.81371" id="Line"></path>
+          </g>
+      </g>
+  </svg>
+)

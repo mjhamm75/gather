df5de728a810a4a99fa1f82d32ac828f515546d9
mjhamm75 <jason.hamm@ldschurch.org>
2016-11-10 21:43:28 -0700

    Add styling for expenses

--- components/Expense.js
+++ components/Expense.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
+import Check from './Check';
 
 import s from './Expense.css';
 
@@ -11,15 +12,17 @@ export default ({title, options}) => {
         options={options}
         placeholder="- Request documents -"
       />
+    <div className={s.grouping}>
       <div className={s.group}>
-        <input type="checkbox"/><label>Mortgage</label>
+        <span className={s.check}><Check /></span><label>Mortgage</label>
       </div>
       <div className={s.group}>
-        <input type="checkbox"/><label>Car Payment</label>
+        <span className={s.check}><Check /></span><label>Car Payment</label>
       </div>
       <div className={s.group}>
-        <input type="checkbox"/><label>Utilities</label>
+        <span className={s.check}><Check /></span><label>Utilities</label>
       </div>
+    </div>
     </div>
   )
 }

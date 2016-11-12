313184096f266cab9fea6973ab1e5126d69a596b
mjhamm75 <jason.hamm@ldschurch.org>
2016-11-12 11:52:01 -0700

    Add title to client info

--- components/ClientInfo.js
+++ components/ClientInfo.js
@@ -11,76 +11,82 @@ const log = state => {
 
 export default () => (
   <div className={s.clientInfo}>
-    <div className={s.column}>
-      <div className={s.group}>
-        <label>Name</label>
-        <input />
-      </div>
-      <div className={s.group}>
-        <label>Street Address</label>
-        <input />
-        <input />
-      </div>
-      <div className={s.cityGroup}>
-        <div className={s.group}>
-          <label>City</label>
-          <input />
-        </div>
-        <div className={s.group}>
-          <label>State</label>
-          <Select
-            options={states}
-            onChange={log}
-          />
-        </div>
-        <div className={s.group}>
-          <label>Zip</label>
-          <input />
-        </div>
-      </div>
-      <div className={s.group}>
-        <label>Phone Number</label>
-        <input />
-      </div>
-      <div className={s.group}>
-        <label>Email Address</label>
-        <input />
-      </div>
+    <div className={s.header}>
+      <div className={s.title}>Account Information</div>
+      <div>+ Add Responsible Parties</div>
     </div>
-    <div className={s.column}>
-      <div className={s.group}>
-        <label>Name</label>
-        <input />
-      </div>
-      <div className={s.group}>
-        <label>Street Address</label>
-        <input />
-        <input />
-      </div>
-      <div className={s.cityGroup}>
+    <div className={s.columns}>
+      <div className={s.column}>
         <div className={s.group}>
-          <label>City</label>
+          <label>Name</label>
           <input />
         </div>
         <div className={s.group}>
-          <label>State</label>
-          <Select
-            options={states}
-            onChange={log}
-          />
+          <label>Street Address</label>
+          <input />
+          <input />
+        </div>
+        <div className={s.cityGroup}>
+          <div className={s.group}>
+            <label>City</label>
+            <input />
+          </div>
+          <div className={s.group}>
+            <label>State</label>
+            <Select
+              options={states}
+              onChange={log}
+              />
+          </div>
+          <div className={s.group}>
+            <label>Zip</label>
+            <input />
+          </div>
         </div>
         <div className={s.group}>
-          <label>Zip</label>
+          <label>Phone Number</label>
+          <input />
+        </div>
+        <div className={s.group}>
+          <label>Email Address</label>
           <input />
         </div>
       </div>
-      <div className={s.group}>
-        <label>Phone Number</label>
-        <input />
-      </div>
-      <div className={s.group}>
-        <label>Email Address</label>
-        <input />
+      <div className={s.column}>
+        <div className={s.group}>
+          <label>Name</label>
+          <input />
+        </div>
+        <div className={s.group}>
+          <label>Street Address</label>
+          <input />
+          <input />
+        </div>
+        <div className={s.cityGroup}>
+          <div className={s.group}>
+            <label>City</label>
+            <input />
+          </div>
+          <div className={s.group}>
+            <label>State</label>
+            <Select
+              options={states}
+              onChange={log}
+              />
+          </div>
+          <div className={s.group}>
+            <label>Zip</label>
+            <input />
+          </div>
+        </div>
+        <div className={s.group}>
+          <label>Phone Number</label>
+          <input />
+        </div>
+        <div className={s.group}>
+          <label>Email Address</label>
+          <input />
+        </div>
       </div>
     </div>
   </div>

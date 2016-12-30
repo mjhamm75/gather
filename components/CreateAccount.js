72856e1fa825a7adc827448b9d1f7c43872bb17a
mjhamm75 <jason.hamm@ldschurch.org>
2016-12-29 22:37:14 -0700

    Add styling for first step

--- components/CreateAccount.js
+++ components/CreateAccount.js
@@ -4,16 +4,20 @@ import s from './CreateAccount.css';
 
 export default () => (
   <div className={s.createAccount}>
-    <div>Please confirm email address.</div>
+    <div className={s.title}>Please confirm email address.</div>
     <div className={s.email}>
-      <label>Email Address</label>
+      <label className={s.label}>Email Address</label>
       <input/>
     </div>
-    <div>Choose a password with at least 8 characters</div>
+    <div className={s.title}>Choose a password with at least 8 characters</div>
     <div className={s.password}>
-      <label>Password</label>
-      <input/>
-      <input/>
+      <label className={s.label}>Password</label>
+      <input className={s.passwordInput}
+        placeholder="Password"
+      />
+      <input className={s.passwordInput}
+        placeholder="Confirm password"
+      />
     </div>
   </div>
 )

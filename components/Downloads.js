cf4679bbac64e69a1ee8b281197a7ca66e4b96cb
mjhamm75 <jason.hamm@ldschurch.org>
2017-01-02 19:55:49 -0700

    Update downloads component

--- components/Downloads.js
+++ components/Downloads.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Download from './Download';
+import Bell from './Bell';
 import s from './Downloads.css';
 
 export default () => {
   return (
     <div className={s.downloads}>
       <div className={s.content}>
+        <div>Please provide the following documents.  You may come back later and add documents.  We will send a [daily] reminder by [text]<span className={s.reminder}><span className={s.reminderText}>Change reminder settings.</span><Bell /></span></div>
         <div className={s.items}>
           <Download />
           <Download />

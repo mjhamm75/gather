8acd5d2ddbc87c43e4888e52a0e17b33cbbb46ac
mjhamm75 <jason.hamm@ldschurch.org>
2017-01-02 20:05:52 -0700

    Add more styling to downloads page

--- components/Downloads.js
+++ components/Downloads.js
@@ -8,7 +8,14 @@ export default () => {
   return (
     <div className={s.downloads}>
       <div className={s.content}>
-        <div>Please provide the following documents.  You may come back later and add documents.  We will send a [daily] reminder by [text]<span className={s.reminder}><span className={s.reminderText}>Change reminder settings.</span><Bell /></span></div>
+        <span>
+            <span>Please provide the following documents.  You may come back later and add documents.  We will send a [daily] reminder by [text]</span>
+            <span className={s.reminder}>
+              <span className={s.reminderText}>Change reminder settings.</span>
+              <Bell />
+            </span>
+        </span>
+        <div className={s.title}>Monthly Expenses</div>
         <div className={s.items}>
           <Download />
           <Download />
@@ -18,6 +25,17 @@ export default () => {
           <Download />
           <Download />
         </div>
+        <div className={s.title}>Assets</div>
+        <div className={s.items}>
+          <Download />
+          <Download />
+          <Download />
+          <Download />
+          <Download />
+          <Download />
+          <Download />
+        </div>
+
       </div>
     </div>
   )
